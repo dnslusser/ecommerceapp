8a35bbf7ca207c3c3227cc416fdfdda25cc538b7
dnslusser <dslusser@madisoncollege.edu>
2022-05-10 13:35:19 -0500

    Removed and readded amplify add api
    take 2!

--- amplify/backend/function/ecommercefunction22s/src/app.js
+++ amplify/backend/function/ecommercefunction22s/src/app.js
@@ -8,6 +8,7 @@ See the License for the specific language governing permissions and limitations 
 
 
 /* Amplify Params - DO NOT EDIT
+	AUTH_ECOMMERCEAPP_USERPOOLID
 	ENV
 	REGION
 	STORAGE_PRODUCTTABLE_ARN
@@ -18,31 +19,6 @@ Amplify Params - DO NOT EDIT */
 const express = require('express')
 const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-
-/* amplify/backend/function/ecommercefunction/src/app.js */
-
-/* Below the last existing `require` import, add the following
-   imports variables */
-   const AWS = require('aws-sdk')
-   const { v4: uuid } = require('uuid')
-   
-   /* Cognito SDK */
-   const cognito = new
-   AWS.CognitoIdentityServiceProvider({
-     apiVersion: '2016-04-18'
-   })
-   
-   /* Cognito User Pool ID
-   *  This User Pool ID variable will be given to you by the CLI output after
-      adding the category
-   *  This will also be available in the file itself, commented out at the top
-   */
-   var userpoolId = process.env.<your_app_id>
-   
-   // DynamoDB configuration
-   const region = process.env.REGION
-   const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME
-   const docClient = new AWS.DynamoDB.DocumentClient({region})
 
 // declare a new express app
 const app = express()
